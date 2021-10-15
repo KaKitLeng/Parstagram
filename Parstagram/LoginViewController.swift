63ec974b1b06ec9839143a04f94595978f83dafe
KaKitLeng <jasonlengkakit@gmail.com>
2021-10-15 19:24:43 -0400

    completed

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -21,8 +21,21 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
         
+        PFUser.logInWithUsername(inBackground:username, password:password) {
+          (user, error) in
+          if user != nil {
+            // Do stuff after successful login.
+            self.performSegue(withIdentifier: "loginSegue", sender: nil)
+          } else {
+            // The login failed. Check error to see why.
+          }
+        }
+
     }
+    
     
     @IBAction func onSignUp(_ sender: Any) {
         let user = PFUser()
