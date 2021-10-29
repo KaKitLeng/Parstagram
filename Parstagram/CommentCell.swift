b9b0dcb801a35dcc79440abe1395a293281251db
KaKitLeng <jasonlengkakit@gmail.com>
2021-10-29 16:46:39 -0400

    completed

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Ka Kit Leng on 10/29/21.
+//  Copyright © 2021 Ka Kit Leng. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
